341c768d9be160280619be21620161a244dbe523
Susan060 <sushantadhikari060@gmail.com>
2026-05-04 19:59:47 +0545

    chatbot api updated

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const api_Url = "http://localhost:3000/api/chat"
+    const api_Url = "https://bot-assist-khaki.vercel.app/"
 
     const scriptTag = document.currentScript;
     const ownerId = scriptTag.getAttribute("data-owner-id")
